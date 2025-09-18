f5178ffe569e3887139313895f275754b9b41de5
Shashwat Tiwari <tiwarishahswat2016@gmail.com>
2025-09-18 20:37:30 +0530

    Delete backend directory

--- backend/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(403).json({ message: "Access denied. Admins only." });
-  }
-};
